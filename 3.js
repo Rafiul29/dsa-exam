46f908a8ea791a5d107f7fdaec92d940ab77be0c
Rafiul29 <rafiulislam4472@gmail.com>
2023-01-24 21:58:31 +0600

    graph added

--- 3.js
+++ 3.js
@@ -13,4 +13,31 @@ class Graph{
             this.adjacencyList[vertex]=new Set()
         }
     }
+    addEdge(vertex1,vertex2){
+        if(!this.adjacencyList[vertex1]){
+            this.addVertext(vertex1)
+        }if(!this,this.adjacencyList[vertex2]){
+            this.addVertext(vertex2)
+        }
+
+        this.adjacencyList[vertex1].add(vertex2)
+        this.adjacencyList[vertex2].add(vertex1)
+    }
+    display() {
+        for (let vertex in this.adjacencyList) {
+          console.log(vertex + " --> " + [...this.adjacencyList[vertex]]);
+        }
+      }
 }
+// time complexity O(V+E)
+
+const graph =new Graph()
+graph.addVertext("A")
+graph.addVertext("B")
+graph.addVertext("C")
+
+graph.addEdge("A", "B");
+graph.addEdge("C", "A");
+
+console.log(graph)
+graph.display()
